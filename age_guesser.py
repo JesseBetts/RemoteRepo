82607db1fd53d0038cc52f8f4d686887e24943d6
Jesse Betts <jessebetts1603@gmail.com>
2025-01-14 15:29:17 -0700

    first commit

---
+++ age_guesser.py
@@ -0,0 +1,32 @@
+#age guessing machine
+import random
+max = 30
+min = 15
+
+
+print("Please enter your name:")
+name = input()
+print(f"Hello, {name}! I am going to guess your age!")
+
+while True: 
+    rnd = random.randint(min, max)
+    print(f"Are you {rnd} years old?")
+    user_input = input()
+    
+    if user_input == "y":
+        print(f"I win! You are {name} and you are {rnd} years old!!")
+        break
+    else :
+        print("Rats! I will try again.")
+        print(f"Give me a hint. Are you older or younger than {rnd}?")
+        hint =input()
+        if hint == "older":
+            min = rnd
+        elif hint == "younger":
+            max = rnd
+    #     else:
+    #         print("Invalid input try again")
+    # else: 
+    #     print("Invalid input try again")
+    # :)       
+        
